36b4a2568ab13bf763a651f5ad50f7afbdfd815f
KavishkaSasindu <sasindu0215@gmail.com>
2024-03-09 21:45:03 +0530

    initialize commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div>HELLO User</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/SIgnUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SIgnUp = () => {
+  return (
+    <div>
+      <div>SignUp page</div>
+    </div>
+  );
+};
+
+export default SIgnUp;
